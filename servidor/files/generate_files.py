fbab47cfde47e382ab94b266e4df740305da1e56
nortegac <n.ortegac@uniandes.edu.co>
2021-09-30 23:45:53 -0500

    Update generate_files.py

--- servidor/files/generate_files.py
+++ servidor/files/generate_files.py
@@ -1,7 +1,6 @@
 texto = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus sem tortor, et ullamcorper enim auctor vel. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris vel bibendum augue, ut iaculis lectus. Vivamus imperdiet fermentum arcu vel molestie. Praesent a scelerisque mauris. Fusce ut urna aliquam, iaculis nibh elementum, finibus mauris. Phasellus gravida ornare tortor pellentesque pharetra. Mauris vestibulum vestibulum nisl, non lacinia lacus tempus sed. Quisque rhoncus, arcu in malesuada porta, metus sapien maximus augue, non mollis mi purus id leo. Duis felis arcu, euismod sit amet egestas vel, facilisis at massa. Aenean euismod, dui at viverra cursus, odio arcu iaculis lacus, vitae semper metus lorem in justo."
 
 text_size = len(texto)
-print(text_size)
 
 fh = open("100MB.txt", "wb")
 num_bytes = 0
@@ -9,6 +8,7 @@ while num_bytes < 100000000:
     fh.write(texto)
     num_bytes += text_size
 fh.close()
+print("Created 100 MB file")
 
 
 fh = open("250MB.txt", "wb")
@@ -17,3 +17,4 @@ while num_bytes < 250000000:
     fh.write(texto)
     num_bytes += text_size
 fh.close()
+print("Created 250 MB file")
